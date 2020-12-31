39607896b8ca915b7fd795f591011ab9109f5ee9
Andrew Kamal <kamalandrew55@gmail.com>
2020-12-30 22:30:48 -0500

    Update 005-addCrypto.js

--- migrations/005-addCrypto.js
+++ migrations/005-addCrypto.js
@@ -2,12 +2,9 @@ var db = require('./db')
 
 exports.up = function (next) {
   var sqls = [
-    'alter table transactions alter satoshis TYPE bigint',
-    "alter table transactions add crypto_code text default 'BTC'",
-    "alter table pending_transactions add crypto_code text default 'BTC'",
-    'alter table pending_transactions alter satoshis TYPE bigint',
-    "alter table bills add crypto_code text default 'BTC'",
-    'alter table bills alter satoshis TYPE bigint'
+    "alter table transactions add crypto_code text default 'LNR'",
+    "alter table pending_transactions add crypto_code text default 'LNR'",
+    "alter table bills add crypto_code text default 'LNR'",
   ]
 
   db.multi(sqls, next)
