85185c5f564d53d275ae83ff31f28f5438d9a2a4
Andrew Kamal <kamalandrew55@gmail.com>
2021-01-01 22:55:38 -0500

    Update strike.js

--- dev/strike.js
+++ dev/strike.js
@@ -3,13 +3,13 @@ const BN = require('../lib/bn')
 
 const account = {token: 'xxx'}
 
-strike.newAddress(account, {cryptoCode: 'BTC', cryptoAtoms: BN(10000)})
+strike.newAddress(account, {cryptoCode: 'LNR', cryptoAtoms: BN(10000)})
   .then(r => {
     console.log(r)
 
     const toAddress = r
     const requested = null
-    const cryptoCode = 'BTC'
+    const cryptoCode = 'LNR'
 
     setInterval(() => {
       strike.getStatus(account, toAddress, requested, cryptoCode)
