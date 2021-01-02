7371e2bd5e9f2421e45283a93a680240ca5c5d79
Andrew Kamal <kamalandrew55@gmail.com>
2021-01-02 11:59:06 -0500

    Update coin.js

--- new-lamassu-admin/src/utils/coin.js
+++ new-lamassu-admin/src/utils/coin.js
@@ -2,39 +2,9 @@ import * as R from 'ramda'
 
 const CRYPTO_CURRENCIES = [
   {
-    cryptoCode: 'BTC',
-    display: 'Bitcoin',
-    code: 'bitcoin',
-    unitScale: 8
-  },
-  {
-    cryptoCode: 'ETH',
-    display: 'Ethereum',
-    code: 'ethereum',
-    unitScale: 18
-  },
-  {
-    cryptoCode: 'LTC',
-    display: 'Litecoin',
-    code: 'litecoin',
-    unitScale: 8
-  },
-  {
-    cryptoCode: 'DASH',
-    display: 'Dash',
-    code: 'dash',
-    unitScale: 8
-  },
-  {
-    cryptoCode: 'ZEC',
-    display: 'Zcash',
-    code: 'zcash',
-    unitScale: 8
-  },
-  {
-    cryptoCode: 'BCH',
-    display: 'Bitcoin Cash',
-    code: 'bitcoincash',
+    cryptoCode: 'LNR',
+    display: 'Lonero',
+    code: 'lonero',
     unitScale: 8
   }
 ]
@@ -53,7 +23,7 @@ function toUnit(cryptoAtoms, cryptoCode) {
 }
 
 function formatCryptoAddress(cryptoCode = '', address = '') {
-  return cryptoCode === 'BCH' ? address.replace('bitcoincash:', '') : address
+  return cryptoCode === 'LNR' ? address.replace('lonero:', '') : address
 }
 
 export { toUnit, formatCryptoAddress }
